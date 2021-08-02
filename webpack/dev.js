09375a8560e42157eb5d232dcf1d670a35e063af
abcd <rrr@gmail.com>
2021-08-02 11:10:29 +0530

    first commit

---
+++ webpack/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'AWS_REGION': 'us-east-1',
+  'USER_POOL_ID': 'us-east-1_wgXmzPyNY',
+  'USER_POOL_CLIENT_ID': '75pkgn2if6acmmj1dgtajluv4n',
+  'IDENTITY_POOL_ID': 'us-east-1:24a0437b-1f53-4dd6-8572-c78f450320c6',
+  'FILE_BUCKET_NAME': 'mystack-filebucket-18ngac4qttes',
+}
